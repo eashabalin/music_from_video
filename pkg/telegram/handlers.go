50f358e1ae813cb7c577f18ff134aa52e7293def
eashabalin <egorsabalin@MacBook-Pro-Egor-2.local>
2023-02-15 14:02:30 +0300

    fixed concurrent downloading

--- pkg/telegram/handlers.go
+++ pkg/telegram/handlers.go
@@ -18,42 +18,24 @@ func (b *Bot) handleUpdate(update tgbotapi.Update) error {
 
 func (b *Bot) handleMessage(message *tgbotapi.Message) error {
 	if message.IsCommand() && message.Command() == "start" {
-		if err := b.sendMessage(message.Chat.ID, "Привет! Отправь ссылку на видео на YouTube, из которого хочешь достать музыку."); err != nil {
+		if err := b.handleStartCommand(message.Chat.ID); err != nil {
 			return err
 		}
-		return nil
 	}
-
-	errChan := make(chan error, 1)
 
 	if b.downloader.IsValidURL(message.Text) {
 		go func() {
-			err := b.sendMessage(message.Chat.ID, "Загрузка...")
-			if err != nil {
-				errChan <- err
-				return
-			}
-			filename, err := b.downloader.Download(message.Text)
+			errChan := make(chan error, 1)
+			go b.handleURL(message.Chat.ID, message.Text, errChan)
+			err := <-errChan
+			fmt.Println("error: ", err)
 			if err == downloader.ErrorDurationTooLong {
-				err = b.sendMessage(message.Chat.ID, "Видео должно быть короче 10 минут.")
-				if err != nil {
-					errChan <- err
-					return
-				}
+				b.sendMessage(message.Chat.ID, "Видео должно быть короче 10 минут.")
 				return
 			}
-			if err != nil {
-				errChan <- fmt.Errorf("failed to download: %w\n", err)
-				return
-			}
-			if err = b.sendAudio(message.Chat.ID, filename); err != nil {
-				errChan <- fmt.Errorf("failed to send audio: %w\n", err)
-				return
-			}
-			errChan <- nil
-			close(errChan)
+			b.sendMessage(message.Chat.ID, "Не получается обработать эту ссылку.")
 		}()
-		return <-errChan
+		return nil
 	}
 
 	if err := b.sendMessage(message.Chat.ID, "Не могу прочитать ссылку на видео."); err != nil {
@@ -61,4 +43,35 @@ func (b *Bot) handleMessage(message *tgbotapi.Message) error {
 	}
 
 	return nil
+}
+
+func (b *Bot) handleStartCommand(chatID int64) error {
+	if err := b.sendMessage(chatID, "Привет! Отправь ссылку на видео на YouTube, из которого хочешь достать музыку."); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (b *Bot) handleURL(chatID int64, url string, errChan chan error) {
+	defer close(errChan)
+	err := b.sendMessage(chatID, "Загрузка...")
+	if err != nil {
+		errChan <- err
+		return
+	}
+	filename, err := b.downloader.Download(url)
+	if err != nil {
+		errChan <- err
+		return
+	}
+	if err != nil {
+		errChan <- fmt.Errorf("failed to download: %w\n", err)
+		return
+	}
+	if err = b.sendAudio(chatID, filename); err != nil {
+		errChan <- fmt.Errorf("failed to send audio: %w\n", err)
+		return
+	}
+	errChan <- nil
+	close(errChan)
 }
